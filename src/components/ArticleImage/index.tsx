3177c5ab8cac721bd46de472e334050766884fde
Iaan Lamanosau <iaan.lamanosau@gmail.com>
2023-10-18 20:14:15 +0300

    feat: add image caption saying to zoom on mobiles

--- src/components/ArticleImage/index.tsx
+++ src/components/ArticleImage/index.tsx
@@ -1,9 +1,15 @@
 import ModalImage from "react-modal-image";
 import { ComponentType, useEffect, useState } from "react";
+import { Typography } from "@mui/material";
+
+import { useBreakpoints } from "@shared";
 
 export const ArticleImage: ComponentType<HTMLImageElement> = (props) => {
   // https://github.com/facebook/react/issues/25627
   const [isMounted, setIsMounted] = useState(false);
+
+  const { isSm, isXs } = useBreakpoints();
+  const isMobile = isSm || isXs;
 
   useEffect(() => {
     setIsMounted(true);
@@ -13,14 +19,45 @@ export const ArticleImage: ComponentType<HTMLImageElement> = (props) => {
     };
   }, []);
 
+  useEffect(() => {
+    const listener = (e: Event) => {
+      if (isMobile) {
+        e.stopPropagation();
+      }
+    };
+
+    const selectors = document.querySelectorAll(".article__modal-image");
+
+    selectors.forEach((sel) => sel.addEventListener("click", listener));
+
+    return () => {
+      selectors.forEach((sel) => sel.removeEventListener("click", listener));
+    };
+  }, [isMobile]);
+
   return isMounted ? (
     <div
       style={{
         zIndex: 10000,
+        display: "flex",
+        flexFlow: "column",
       }}
       suppressHydrationWarning
     >
-      <ModalImage small={props.src} large={props.src} alt={props.alt} />
+      <ModalImage
+        className="article__modal-image"
+        medium={props.src}
+        small={props.src}
+        large={props.src}
+        alt={props.alt}
+      />
+      {isMobile && (
+        <Typography
+          sx={{ fontSize: "10px", textAlign: "center", color: "grey" }}
+        >
+          Pinch fingers to zoom
+        </Typography>
+      )}
     </div>
   ) : null;
 };
